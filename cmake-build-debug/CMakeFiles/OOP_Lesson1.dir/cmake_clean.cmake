f2ad47e36aa6cb7d9611d2e3065f4f80ff1fe52d
Eugen0 <leahu_eugen@yahoo.com>
2020-05-15 18:16:49 +0100

    done

---
+++ cmake-build-debug/CMakeFiles/OOP_Lesson1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Lesson1.dir/main.cpp.obj"
+  "OOP_Lesson1.exe"
+  "OOP_Lesson1.exe.manifest"
+  "OOP_Lesson1.pdb"
+  "libOOP_Lesson1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Lesson1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
