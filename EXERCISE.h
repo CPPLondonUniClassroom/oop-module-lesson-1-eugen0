4fcb20732ec857adc52fd506e7a277ea292e8193
Eugen0 <leahu_eugen@yahoo.com>
2020-05-15 18:15:30 +0100

    done

--- EXERCISE.h
+++ EXERCISE.h
@@ -1,5 +1,25 @@
 #pragma once
-
+# include <tuple>
 class Student {
-    //Write your code in here.
+protected:
+    std::string firstName;
+    std::string lastName;
+
+public:
+    // constructors;
+    Student(){firstName = "Unknown"; lastName = "Unknown"; }                                                  // default
+    Student(std::string fname, std::string lname): firstName{std::move(fname)}, lastName{std::move(lname)}{}; // with arguments;
+
+    // member functions;
+    const std::string& FirstName() const {return this->firstName;};
+    const std::string& LastName() const {return this->lastName;};
+
+    // setters
+    Student& FirstName(std::string fname){ firstName = {std::move(fname)}; return *this;};
+    Student& LastName(std::string lname) { lastName = {std::move(lname)}; return *this;};
+    // comparison operator
+    bool operator==(const Student& ex) const  {
+       // return this->firstName == ex.firstName && this->lastName == ex.lastName; };
+       return std::tie(this->firstName, this->lastName) == std::tie(ex.firstName, ex.lastName);}; // use of tie
 };
+
